281e4f622f5192815ebe8f017c21312b09b7d14f
Ori Gold <nehoraigold@gmail.com>
2019-01-02 15:31:02 +0200

    js function to get coordinates from address working

--- frontend/js/logic.js
+++ frontend/js/logic.js
@@ -1,3 +1,6 @@
+var MAPBOX_KEY = 'sk.eyJ1IjoibmVob3JhaWdvbGQiLCJhIjoiY2pxZjVmYzh6NGxzODQybGN5bTlwNHBpeiJ9.ruE0eqcqGrt8_hfv3VOGBg';
+var MAPQUEST_API_KEY = "VpY2AF2afXCdfAKEBPGgxnv0tRsF1Rnk";
+
 var map = L.map('map').setView([32.0853, 34.7818], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -6,3 +9,23 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'sk.eyJ1IjoibmVob3JhaWdvbGQiLCJhIjoiY2pxZjVmYzh6NGxzODQybGN5bTlwNHBpeiJ9.ruE0eqcqGrt8_hfv3VOGBg'
 }).addTo(map);
+
+function getLongLatFromAddress(address) {
+    $.ajax({
+        type: "POST",
+        url: `http://www.mapquestapi.com/geocoding/v1/address?key=${MAPQUEST_API_KEY}`,
+        data: {
+            "location": address
+        },
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function(resp) {
+            var lat = resp.results[0].locations[0].latLng.lat;
+            var lng = resp.results[0].locations[0].latLng.lng;
+            return [lat, lng];
+        },
+        error: function(resp) {
+            console.log(resp);
+        }
+    })
+}
