d29753ed2705d10fa1eae31202a95f41d2fb8c34
Tom Cohen <tomcohen0079@gmail.com>
2019-01-03 10:40:34 +0200

    updated json

--- backend/database_hackathon19.py
+++ backend/database_hackathon19.py
@@ -6,28 +6,17 @@ DB_FILE = 'jpark.db'
 
 class MapsDb:
     def __init__(self):
-        # CREATE_POLY = """CREATE TABLE IF NOT EXISTS Polygons (ID INTEGER PRIMARY KEY AUTOINCREMENT, info TEXT  NOT NULL);"""
 
         CREATE_PARK = """CREATE TABLE IF NOT EXISTS Parking (ID INTEGER PRIMARY KEY AUTOINCREMENT,lat DECIMAL(10, 8) 
         NOT NULL,lng DECIMAL(11, 8) NOT NULL, datetime TEXT NOT NULL);"""
 
         conn = sqlite3.connect(DB_FILE)
         c = conn.cursor()
-        # c.execute(CREATE_POLY)
         c.execute(CREATE_PARK)
         conn.commit()
         c.close()
 
-    # def insert_poly(self, Polynome):
-    #     """insert one polygome to table"""
-    #
-    #     sql = ''' INSERT INTO Polygons(info) VALUES(?) '''
-    #
-    #     conn = sqlite3.connect(DB_FILE)
-    #     c = conn.cursor()
-    #     c.execute(sql, Polynome)
-    #     conn.commit()
-    #     c.close()
+
 
     def insert_point(self, point):
         """insert a data point """
@@ -42,4 +31,4 @@ class MapsDb:
 if __name__ == '__main__':
     database = MapsDb()
 
-    # database.insert_poly()
+    # database.insert_point()
--- backend/polygons_coordinates/polygons_to_json.py
+++ backend/polygons_coordinates/polygons_to_json.py
@@ -20,9 +20,16 @@ def insert_to_json(coords, name):
     :param name: name of polygone
     :return:
     """
-    with open(FILE, 'r') as f:
-        data = json.load(f)
-    poly = {'Name': str(name), 'coords': coords}
+    try:
+        with open(FILE, 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        with open(FILE, 'x') as f:
+            data = []
+
+    index = len(data) + 1
+
+    poly = {'Name': str(name), 'index': index, 'coords': coords}
     data.append(poly)
 
     with open(FILE, 'w') as f:
@@ -30,10 +37,12 @@ def insert_to_json(coords, name):
 
 
 if __name__ == '__main__':
-    polygon_points = [32.101537, 34.78054, 32.101464, 34.779741, 32.099883, 34.779811, 32.09976, 34.780551, 32.099469,
-                      34.780717, 32.099142, 34.780674, 32.09897, 34.781774, 32.099142, 34.782085, 32.100192, 34.781887,
-                      32.100174, 34.781372, 32.101542, 34.780889]  # input coordinates
-    name = 'north_tlv'  # input name
+    polygon_points = [32.051354, 34.771843, 32.051518, 34.77194, 32.051591, 34.77165, 32.052473, 34.771682, 32.05245,
+                      34.772112, 32.052655, 34.772096, 32.052677, 34.771752, 32.053259, 34.771795, 32.053605, 34.770894,
+                      32.053741, 34.770712, 32.053878, 34.770776, 32.05405, 34.768373, 32.052232, 34.767804, 32.051823,
+                      34.769209, 32.051513, 34.770046, 32.050859, 34.771484]
+
+    name = 'gina'  # input name
 
     c_coords = coord_in_lat_long(polygon_points)
 
